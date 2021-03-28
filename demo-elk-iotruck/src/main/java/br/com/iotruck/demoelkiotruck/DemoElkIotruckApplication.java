6b8cf82fbb73108a58fe3599da8f0fff0cbaadfc
jose-lucas-mq <jose.queiroz@bandtec.com.br>
2021-03-27 23:51:45 -0300

    elk test

---
+++ demo-elk-iotruck/src/main/java/br/com/iotruck/demoelkiotruck/DemoElkIotruckApplication.java
@@ -0,0 +1,13 @@
+package br.com.iotruck.demoelkiotruck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoElkIotruckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoElkIotruckApplication.class, args);
+	}
+
+}
---
+++ demo-elk-iotruck/src/test/java/br/com/iotruck/demoelkiotruck/DemoElkIotruckApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.iotruck.demoelkiotruck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoElkIotruckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
